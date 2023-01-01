7b5f48839e697830478e09b4dbf837b7735daae3
Yumacide <yumanomics@gmail.com>
2023-01-01 20:25:28 +0400

    Optimize and refactor

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
-use fast_image_resize::{self as fr, Image, PixelType};
+use mtpng::{
+    encoder::{Encoder, Options},
+    Header,
+};
 use screenshots::Screen;
 use std::{
-    io::Write,
+    io::{BufReader, BufWriter, Read, Write},
     net::{TcpListener, TcpStream},
-    num::NonZeroU32,
     sync::{Arc, Mutex},
     thread, time,
 };
+
+use resize::Pixel::RGBA8;
+use resize::Type::Lanczos3;
+use rgb::FromSlice;
+
+const WIDTH: u32 = 256;
+const HEIGHT: u32 = 144;
+
+// FIX: make screenshots library output rgb instead of rgba
 
 fn main() {
     let screen = Screen::all().unwrap()[0];
@@ -14,23 +25,25 @@ fn main() {
     {
         let current_img = Arc::clone(&current_img);
         thread::spawn(move || loop {
-            let _img = screen.capture().unwrap();
+            let start = time::Instant::now();
+            let img = screen.capture().unwrap();
+            let img = resize(
+                img.buffer(),
+                img.width() as usize,
+                img.height() as usize,
+                WIDTH as usize,
+                HEIGHT as usize,
+            );
 
-            let mut img = Image::from_vec_u8(
-                NonZeroU32::new(_img.width()).unwrap(),
-                NonZeroU32::new(_img.height()).unwrap(),
-                _img.buffer().to_vec(),
-                PixelType::U8x4,
-            )
-            .unwrap();
+            let img_rgb = to_rgb(img);
+            current_img.lock().unwrap().clear();
+            png_encode(&img_rgb, &mut *current_img.lock().unwrap());
 
-            let dst_width = NonZeroU32::new(192).unwrap();
-            let dst_height = NonZeroU32::new(108).unwrap();
-            let img = resize(&mut img, dst_width, dst_height);
-
-            *current_img.lock().unwrap() = img.buffer().to_vec();
-
-            thread::sleep(time::Duration::from_millis(100));
+            if start.elapsed().as_secs_f64() < 0.05 {
+                thread::sleep(time::Duration::from_secs_f64(
+                    0.05 - start.elapsed().as_secs_f64(),
+                ));
+            }
         });
     }
 
@@ -38,50 +51,61 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream, current_img.lock().unwrap().to_vec());
+        handle_connection(stream, &current_img.lock().unwrap());
     }
 }
 
-fn resize<'a>(
-    src_image: &'a mut Image<'a>,
-    dst_width: NonZeroU32,
-    dst_height: NonZeroU32,
-) -> Image<'a> {
-    // Multiple RGB channels of source image by alpha channel
-    // (not required for the Nearest algorithm)
-    let alpha_mul_div = fr::MulDiv::default();
-    alpha_mul_div
-        .multiply_alpha_inplace(&mut src_image.view_mut())
+fn to_rgb(img: Vec<u8>) -> Vec<u8> {
+    let mut rgb = vec![0; (WIDTH * HEIGHT * 3) as usize];
+    for (output, chunk) in rgb.chunks_exact_mut(3).zip(img.chunks_exact(4)) {
+        output.copy_from_slice(&chunk[0..3]);
+    }
+    rgb
+}
+
+fn png_encode<W: Write>(img: &[u8], dst: W) {
+    let mut header = Header::new();
+    header.set_size(WIDTH, HEIGHT).unwrap();
+    header.set_color(mtpng::ColorType::Truecolor, 8).unwrap();
+
+    let mut options = Options::new();
+    options
+        .set_compression_level(mtpng::CompressionLevel::Fast)
         .unwrap();
 
-    // Create container for data of destination image
-    let mut dst_image = fr::Image::new(dst_width, dst_height, src_image.pixel_type());
+    let mut encoder = Encoder::new(dst, &options);
 
-    // Get mutable view of destination image data
-    let mut dst_view = dst_image.view_mut();
+    encoder.write_header(&header).unwrap();
+    encoder.write_image_rows(img).unwrap();
+    encoder.finish().unwrap();
+}
 
-    // Create Resizer instance and resize source image
-    // into buffer of destination image
-    let mut resizer = fr::Resizer::new(fr::ResizeAlg::Convolution(fr::FilterType::Lanczos3));
-    resizer.resize(&src_image.view(), &mut dst_view).unwrap();
-
-    // Divide RGB channels of destination image by alpha
-    alpha_mul_div.divide_alpha_inplace(&mut dst_view).unwrap();
+fn resize(
+    src_image: &[u8],
+    src_width: usize,
+    src_height: usize,
+    dst_width: usize,
+    dst_height: usize,
+) -> Vec<u8> {
+    let mut dst_image = vec![0; dst_width * dst_height * 4];
+    let mut resizer = resize::new(
+        src_width, src_height, dst_width, dst_height, RGBA8, Lanczos3,
+    )
+    .unwrap();
+    resizer
+        .resize(src_image.as_rgba(), dst_image.as_rgba_mut())
+        .unwrap();
     dst_image
 }
 
-fn handle_connection(mut stream: TcpStream, img: Vec<u8>) {
-    let mut str = String::new();
-    let mut i = 0;
-    for n in img.iter() {
-        i += 1;
-        if i % 4 == 0 {
-            continue;
-        }
-        str.push(*n as char);
-    }
+fn handle_connection(mut stream: TcpStream, img: &[u8]) {
+    let mut buf = [0];
+    let mut buf_reader = BufReader::new(&mut stream);
+    buf_reader.read_exact(&mut buf).unwrap();
 
-    let response = format!("HTTP/1.1 200 OK \r\n\r\n{}", str);
-    stream.write_all(response.as_bytes()).unwrap();
-    println!("{} pixels", str.len() / 3);
+    stream
+        .write_all("HTTP/1.1 200 OK \r\n\r\n".as_bytes())
+        .unwrap();
+    let mut buf = BufWriter::new(stream);
+    buf.write_all(img).unwrap();
 }
